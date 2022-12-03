cb55f2038d9ebbe332ad42ec1e492de00ed22e59
mechztcc <dojathan@gmail.com>
2022-12-03 20:35:11 -0300

    feat: added celebrate to validate body content

--- src/modules/user/routes/users.routes.ts
+++ src/modules/user/routes/users.routes.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+
 import { UsersController } from '../controllers/UsersController';
 
 const usersController = new UsersController();
 
 const usersRouter = Router();
 
-usersRouter.post('/create', usersController.create);
+usersRouter.post(
+  '/create',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+      role: Joi.string().required(),
+    },
+  }),
+  usersController.create
+);
 
 export default usersRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,34 @@
 import 'reflect-metadata';
 import 'express-async-errors';
-require('dotenv').config()
 import '@shared/typeorm/';
 
-import routes from '@shared/routes/index'
-import express, { NextFunction, Request, Response } from 'express';
-import AppError from './config/errors/AppError';
+require('dotenv').config();
 
+import routes from '@shared/routes/index';
+import express, { NextFunction, Request, Response } from 'express';
+import { errors } from 'celebrate';
+
+import AppError from './config/errors/AppError';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use(errors());
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
     return res
       .status(error.statusCode)
       .json({ status: 'error', message: error.message });
   } else {
-    console.log(error)
+    console.log(error.name);
     return res
       .status(500)
       .json({ status: 'error', message: 'Internal server error' });
   }
 });
 
-
-const port = process.env.API_PORT
+const port = process.env.API_PORT;
 app.listen(port, () => {
   console.log(`Server running at localhost:${port}`);
 });
-
-
