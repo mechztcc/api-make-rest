a34be7e1e519f0285be87904a9b6e753b2ce863e
mechztcc <dojathan@gmail.com>
2022-12-04 08:42:14 -0300

    feat: added isAuth middleware

---
+++ src/shared/middlewares/isAuth.ts
@@ -0,0 +1,40 @@
+import authConfig from '@config/auth/auth';
+
+import AppError from '@shared/middlewares/isAuth';
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function isAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT Token is missing');
+  }
+
+  // Bearer huehdkaksu12312jjdkau2312
+  const [bearer, token] = authHeader.split(' ');
+
+  try {
+    const decodedToken = verify(token, authConfig.jwt.secret);
+    // sub is a Id of user
+    // Override type Request at src/@types/express/index.d.ts
+    // The main object of this changes, is turn visible the id of user, to all request
+    // That this method intercep
+    const { sub } = decodedToken as ITokenPayload;
+    request.user = { id: sub };
+
+    return next();
+  } catch (error) {
+    throw new AppError('Invalid JWT Token.');
+  }
+}
