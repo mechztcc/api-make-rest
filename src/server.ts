d6b3b56208eb7a3793a777f6c33f28e124288d4d
mechztcc <dojathan@gmail.com>
2022-12-11 10:41:20 -0300

    feat: enabled CORS

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { config } from 'dotenv';
 import routes from '@shared/routes/index';
 import express, { NextFunction, Request, Response } from 'express';
 import { errors, isCelebrateError } from 'celebrate';
+import cors from 'cors';
 
 import AppError from './config/errors/AppError';
 
@@ -13,6 +14,7 @@ config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use(errors());
@@ -20,8 +22,8 @@ app.use(errors());
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
     return res
-    .status(error.statusCode)
-    .json({ statusCode: error.statusCode, message: error.message });
+      .status(error.statusCode)
+      .json({ statusCode: error.statusCode, message: error.message });
   } else {
     console.log(error);
     return res
