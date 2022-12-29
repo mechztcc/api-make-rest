c19a1b93660f94ee0ea5c122e8caee544d9687c8
mechztcc <dojathan@gmail.com>
2022-12-29 13:00:00 -0300

    feat: added new test on createuser

--- src/modules/users/services/CreateUserService/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService/CreateUserService.spec.ts
@@ -1,6 +1,9 @@
+import AppError from '@config/errors/AppError';
 import { faker } from '@faker-js/faker';
 import { FakeUsersRepository } from '@modules/users/domain/repositories/fakes/FakeUsersRepository';
+import { User } from '@modules/users/typeorm/entities/User';
 import { Roles } from '@shared/types/Roles';
+import exp from 'constants';
 import { CreateUserService } from './CreateUserService';
 
 describe('Create user', () => {
@@ -17,5 +20,32 @@ describe('Create user', () => {
     });
 
     expect(user).toHaveProperty('id');
+    expect(typeof user.password).toBe('string');
+  });
+
+  it('It should be throw new AppError when try to create user with already in use email', async () => {
+    const fakerUsersRepository = new FakeUsersRepository();
+
+    const createUser = new CreateUserService(fakerUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'User 01',
+      email: faker.internet.email(),
+      password: '123456',
+      role: Roles.ADMIN,
+    });
+
+    await createUser
+      .execute({
+        name: 'User 01',
+        email: user.email,
+        password: '123456',
+        role: Roles.ADMIN,
+      })
+      .catch((error) => {
+        expect(error).toBeInstanceOf(AppError);
+        expect(error).toHaveProperty('message', 'Email address already used.');
+        expect(error).toHaveProperty('statusCode', 400);
+      });
   });
 });
--- src/modules/users/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -1,6 +1,6 @@
 import { ICreateUser } from '@modules/users/domain/models/CreateUser.interface';
 import { IUser } from '@modules/users/domain/models/Users.interface';
-import { IUsersRepository } from '@modules/users/domain/repositories/UsersRepository.interface';
+import { IUsersRepository } from '@modules/users/domain/repositories/interfaces/UsersRepository.interface';
 import { EntityRepository, getRepository, Repository } from 'typeorm';
 import { User } from '../entities/User';
 
@@ -11,7 +11,7 @@ export class UsersRepository implements IUsersRepository {
   constructor() {
     this.repository = getRepository(User);
   }
-  
+
   create({ name, email, password, roles, status }: ICreateUser): IUser {
     const user = this.repository.create({
       name,
@@ -23,8 +23,7 @@ export class UsersRepository implements IUsersRepository {
     return user;
   }
 
-
-  async save(user: IUser): Promise<ICreateUser> {
+  async save(user: IUser): Promise<IUser> {
     const createdUser = await this.repository.save(user);
     return createdUser;
   }
