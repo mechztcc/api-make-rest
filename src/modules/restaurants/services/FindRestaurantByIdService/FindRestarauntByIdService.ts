10fae15d3fafa77e774efa4f56dbd58d3dc914fe
mechztcc <dojathan@gmail.com>
2023-01-05 11:50:52 -0300

    remake: find restaurant by id

--- src/modules/restaurants/services/FindRestaurantByIdService/FindRestarauntByIdService.ts
+++ src/modules/restaurants/services/FindRestaurantByIdService/FindRestarauntByIdService.ts
@@ -1,17 +1,20 @@
 import AppError from '@config/errors/AppError';
+import { IRestaurantsRepository } from '@modules/restaurants/domain/repositories/interfaces/RestaurantsRepository.interface';
 import { Restaurant } from '@modules/restaurants/typeorm/entities/Restaurant';
-import { RestaurantsRepository } from '@modules/restaurants/typeorm/repository/RestaurantsRepository';
-import { getCustomRepository } from 'typeorm';
+import { inject, injectable } from 'tsyringe';
 
 interface IRequest {
   id: number;
 }
-
+@injectable()
 export class FindRestaurantByIdService {
-  async execute({ id }: IRequest): Promise<Restaurant> {
-    const restaurantsRepository = getCustomRepository(RestaurantsRepository);
+  constructor(
+    @inject('RestaurantsRepository')
+    private restaurantsRepository: IRestaurantsRepository
+  ) {}
 
-    const restaurant = await restaurantsRepository.findById(id);
+  async execute({ id }: IRequest): Promise<Restaurant> {
+    const restaurant = await this.restaurantsRepository.findById(id);
     if (!restaurant) {
       throw new AppError('Restaurant not found');
     }
